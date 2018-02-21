4fa61a9fd4f69dd04b723bbe8cb7777135ca1055
Doğan KARTAL <dogan.kartal@markum.com>
2018-02-21 09:45:36 +0300

    Add project files.

---
+++ 0810/Controllers/AnasayfaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0810.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.DateNow = DateTime.Now.Year;
+            return View();
+        }
+    }
+}
